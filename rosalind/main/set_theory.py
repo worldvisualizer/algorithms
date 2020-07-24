9418169b0bc57239b279195ff6e5943d05fd0949
정승우 <seung-woo@homehub.olleh.com>
2020-07-24 10:07:55 +0900

    wip

---
+++ rosalind/main/set_theory.py
@@ -0,0 +1,9 @@
+# import statements
+
+# genotyping: snp markers from human to note migration
+# https://genographic.nationalgeographic.com/
+
+# collection of n characters -> collection of on/off
+
+def number_of_subsets(n):
+    return (2 ** n) / 10000000
